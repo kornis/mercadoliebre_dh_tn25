7abc31d6ae74b2f420a3778ffff68babac3e39d4
Federico Garcia <fedegarcia222@gmail.com>
2021-11-10 20:07:25 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+const port = 3000;
+
+app.listen( process.env.PORT || port , ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
